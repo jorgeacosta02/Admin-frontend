e72c227721bcda7b8bb9e57b2d830038722470fe
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-05-14 19:42:32 -0300

    navBar

--- src/components/navBarLogoComp/NavBarLogoComp.tsx
+++ src/components/navBarLogoComp/NavBarLogoComp.tsx
@@ -1,14 +1,12 @@
 import styles from './_NavBarLogoComp.module.scss';
-import TriarLogo from '../../assets/Images/Logos&Brands/logo fondo transparente.png'
+// import TriarLogo from '../../assets/Images/Logos&Brands/logo fondo transparente.png'
 
 const LogoComp = () => {
   return (
     <div className={styles.container}>
-      <img
-        src={TriarLogo}
-        alt="Exploservice Logo"
-        className={styles.triarLogo}  
-      />
+      <h4 className={styles.brand}>
+        Gestión Residencial Integral
+      </h4>
     </div>
   )
 }
