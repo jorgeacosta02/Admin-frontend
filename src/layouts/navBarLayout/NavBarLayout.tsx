dade11a1416c4b8829a9af757d4e6e737a09d359
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-06-10 09:56:38 -0300

    style

--- src/layouts/navBarLayout/NavBarLayout.tsx
+++ src/layouts/navBarLayout/NavBarLayout.tsx
@@ -1,20 +1,13 @@
 import styles from './_NavBarLayout.module.scss';
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
 import NavBarLogoComp from "../../components/navBarLogoComp/NavBarLogoComp";
 import NavBarNavListComp from "../../components/navBarNavListComp/NavBarNavListComp";
 import NavBarMenuComp from "../../components/navBarMenuComp/NavBarMenuComp";
-import NavBarOptionsComp from "../../components/navBarOptionsComp/NavBarOptionsComp";
-import { selectMoonState } from '../../redux/slices/moonSlice';
 
 
 const NavBarLayout: React.FC = () => {
 
   const [scrollingDown, setScrollingDown] = useState(false);
-
-  const  moonState = useSelector(selectMoonState).moon;
-
-  console.log(moonState)
 
   useEffect(() => {
     const handleScroll = () => {
@@ -31,15 +24,13 @@ const NavBarLayout: React.FC = () => {
 
   const scrollingStyles = `${styles.navBar} ${
     scrollingDown ?
-      moonState ?
-        styles.backgLight :
-        styles.backgDark 
-    : ''}`
+    styles.backgDark:
+    ''
+  }`
 
   return (
     <div className={scrollingStyles}>
       <NavBarLogoComp/>
-      <NavBarOptionsComp/>
       <NavBarMenuComp/>
       <NavBarNavListComp/>
     </div>
