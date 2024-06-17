8b6b9ccd99ad2be23efc699db9cc579c9f8f24fd
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-06-17 17:23:59 -0300

    clientsContainer

--- src/components/clientsComp/ClientsComp.tsx
+++ src/components/clientsComp/ClientsComp.tsx
@@ -1,4 +1,6 @@
+import { NavLink } from 'react-router-dom'
 import styles from './_ClientsComp.module.scss'
+import elbosque from '../../assets/images/links/Portal.jpg'
 
 const ClientsComp = () => {
   return (
@@ -6,6 +8,23 @@ const ClientsComp = () => {
       <h2 className={styles.mainTitle}>
         Clientes
       </h2>
+      <div className={styles.dataContainer}>
+        <div className={styles.clientContainer}>
+            <NavLink
+             to='/elbosque'
+             className={styles.clientLink}
+            >
+                <img
+                    src={elbosque}
+                    alt="Imagen El Bosque"
+                    className={styles.clientImg}
+                />
+                <h5 className={styles.client}>
+                    Complejo Residencial El Bosque
+                </h5>
+            </NavLink>
+        </div>
+      </div>
     </div>
   )
 }
