bd937d039f39b832118b6c17a5fbd6301f4d6ba5
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-06-21 11:28:24 -0300

    styles

--- src/components/homeComp/HomeComp.tsx
+++ src/components/homeComp/HomeComp.tsx
@@ -1,38 +1,24 @@
 import styles from './_HomeComp.module.scss'
-import { useSelector } from 'react-redux';
-import { selectMoonState } from '../../redux/slices/moonSlice';
 import SliderComp from '../sliderComp/SliderComp';
 
 
 const HomeComp = () => {
   
-  const moonState = useSelector(selectMoonState);
-  
-  const dataColor = `${styles.data} ${moonState.moon ? styles.dataWhite : ''}`;
-
-  // const langState = useSelector(selectLangState).lang;
-
   return (
     <div className={styles.mainContainer}>
       <SliderComp/>
       <div className={styles.dataContainer}>
-        <div className={dataColor}>
-          <h5
-            className={styles.role}
-          >
-            Servicio de administración de consorcios eficiente y transparente, entendiendo que la administración de un consorcio requiere una atención meticulosa y un enfoque personalizado.
-          </h5>
-          {/* <p
-            className={styles.text}
-          >
-          </p> */}
-        </div>
+        <h5
+          className={styles.role}
+        >
+          Servicio de administración de consorcios eficiente y transparente, entendiendo que la administración de un consorcio requiere una atención meticulosa y un enfoque personalizado.
+        </h5>
+        <h5
+          className={styles.role}
+        >
+          Toda la información de gestión actualizada y disponible de forma permanente para los miembros del consorcio.
+        </h5>
       </div>
-      {/* <div className={styles.clientsContainer}>
-        <h3 className={styles.title}>
-            Clientes
-        </h3>
-      </div> */}
     </div>
   );
 }
