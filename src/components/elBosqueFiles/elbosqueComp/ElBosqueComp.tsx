a27b08e896cb94d43e70bf69240201fe5fa250f6
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-07-07 12:50:06 -0300

    ElBosque options

--- src/components/elBosqueFiles/elbosqueComp/ElBosqueComp.tsx
+++ src/components/elBosqueFiles/elbosqueComp/ElBosqueComp.tsx
@@ -9,7 +9,7 @@ const ElBosqueComp = () => {
             to='/'
             className={styles.back}
       >
-        Volver
+        Ir a Inicio
       </NavLink>
       <h4 className={styles.mainTitle}>
         Complejo Residencial El Bosque
@@ -25,19 +25,34 @@ const ElBosqueComp = () => {
             className={styles.resumen}
           />
         </div>
+        <h3 className={styles.title}>
+          Secciones
+        </h3>
         <div className={styles.linksContainer}>
-          <NavLink
-            to='/elbosquereglam'
-            className={styles.link}
+          <ul
+            className={styles.links}
           >
-            Reglamento
-          </NavLink>
-          <a
-            className={styles.link}
-            href="https://github.com/jorgeacosta02/Admin-frontend/raw/main/src/assets/ElBosqueFiles/PlanillaElBosque.xlsx" download="PlanillaElBosque.xlsx" target="_blank"
-          >
-            Descargar Planilla
-          </a>
+            <li
+              className={styles.liLink}
+            >
+              <NavLink
+                to='/elbosquereglam'
+                className={styles.link}
+                >
+                Reglamento
+              </NavLink>
+            </li>
+            <li
+              className={styles.liLink}
+            >
+              <a
+                className={styles.link}
+                href="https://github.com/jorgeacosta02/Admin-frontend/raw/main/src/assets/ElBosqueFiles/PlanillaElBosque.xlsx" download="PlanillaElBosque.xlsx" target="_blank"
+                >
+                Descargar Planilla
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
