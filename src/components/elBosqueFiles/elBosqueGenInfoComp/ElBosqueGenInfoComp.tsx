21d67b6e89c6f441d55da0ade91153273ed63305
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-07-10 16:17:07 -0300

    GenInfoComp

---
+++ src/components/elBosqueFiles/elBosqueGenInfoComp/ElBosqueGenInfoComp.tsx
@@ -0,0 +1,15 @@
+import styles from './_ElBosqueGenInfoComp.module.scss';
+
+const ElBosqueGenInfoComp = () => {
+  return (
+    <div 
+        className={styles.mainContainer}
+    >
+      <h3 className={styles.mainTitle}>
+        Información general El Bosque
+      </h3>
+    </div>
+  )
+}
+
+export default ElBosqueGenInfoComp
