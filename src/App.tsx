57d696481b8d09c55526f79b0bb53e95e6c232be
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-07-12 14:12:26 -0300

    ScrollToTopComp

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,47 @@
-import { Routes, Route } from "react-router-dom"
-import HomePage from "./pages/homePage/HomePage"
-import ContactPage from "./pages/contactPage/ContactPage"
-import AdminRoutesComp from "./components/protectedRoutes/adminRoutesComp/AdminRoutesComp"
-// import UserRoutesComp from "./components/protectedRoutes/userRoutesComp/UserRoutesComp"
-import IntranetPage from "./pages/intranetPage/IntranetPage"
-import ArticleUpFormPage from "./pages/articleUpFormPage/ArticleUpFormPage"
-import UserLoginPage from "./pages/userLoginPage/UserLoginPage"
-import ElBosquePage from "./pages/elBosquePage/ElBosquePage"
-import ElBosqueRoutesComp from "./components/protectedRoutes/elBosqueRoutesComp/ElBosqueRoutesComp"
-import ElBosqueReglamPage from "./pages/elBosqueReglamPage/ElBosqueReglamPage"
-import ElBosqueGenInfoPage from "./pages/elBosqueGenInfoPage/ElBosqueGenInfoPage"
-import ElBosqueConsorcistasPage from "./pages/elBosqueConsorcistasPage/ElBosqueConsorcistasPage"
-import ElBosqueAdminPage from "./pages/elBosqueAdminPage/ElBosqueAdminPage"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/homePage/HomePage";
+import ContactPage from "./pages/contactPage/ContactPage";
+import AdminRoutesComp from "./components/protectedRoutes/adminRoutesComp/AdminRoutesComp";
+// import UserRoutesComp from "./components/protectedRoutes/userRoutesComp/UserRoutesComp";
+import IntranetPage from "./pages/intranetPage/IntranetPage";
+import ArticleUpFormPage from "./pages/articleUpFormPage/ArticleUpFormPage";
+import UserLoginPage from "./pages/userLoginPage/UserLoginPage";
+import ElBosquePage from "./pages/elBosquePage/ElBosquePage";
+import ElBosqueRoutesComp from "./components/protectedRoutes/elBosqueRoutesComp/ElBosqueRoutesComp";
+import ElBosqueReglamPage from "./pages/elBosqueReglamPage/ElBosqueReglamPage";
+import ElBosqueGenInfoPage from "./pages/elBosqueGenInfoPage/ElBosqueGenInfoPage";
+import ElBosqueConsorcistasPage from "./pages/elBosqueConsorcistasPage/ElBosqueConsorcistasPage";
+import ElBosqueAdminPage from "./pages/elBosqueAdminPage/ElBosqueAdminPage";
+import ScrollToTop from "./components/scrollToTopComp/ScrollToTopComp";
 
-
-
-function App() {
+const App: React.FC = () => {
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/contact' element={<ContactPage/>}/>
-        <Route path='/login' element={<UserLoginPage/>}/>
-        // Admin Routes
-        <Route element={<AdminRoutesComp/>}>
-          <Route path='/intranet' element={<IntranetPage/>}/>
-          <Route path='/article-up-form' element={<ArticleUpFormPage/>}/>
-        </Route>
-        // El Bosque Routes
-        <Route element={<ElBosqueRoutesComp/>}>
-          <Route path='/elbosque' element={<ElBosquePage/>}/>
-          <Route path='/elbosquereglam' element={<ElBosqueReglamPage/>}/>
-          <Route path='/elbosque-info-general' element={<ElBosqueGenInfoPage/>}/>
-          <Route path='/elbosque-consorcistas' element={<ElBosqueConsorcistasPage/>}/>
-          <Route path='/elbosque-admin' element={<ElBosqueAdminPage/>}/>
-        </Route>
-      </Routes>
-    </div>
-  )
-}
+    <>
+      <ScrollToTop />
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/login" element={<UserLoginPage />} />
+          {/* Admin Routes */}
+          <Route element={<AdminRoutesComp />}>
+            <Route path="/intranet" element={<IntranetPage />} />
+            <Route path="/article-up-form" element={<ArticleUpFormPage />} />
+          </Route>
+          {/* El Bosque Routes */}
+          <Route element={<ElBosqueRoutesComp />}>
+            <Route path="/elbosque" element={<ElBosquePage />} />
+            <Route path="/elbosquereglam" element={<ElBosqueReglamPage />} />
+            <Route path="/elbosque-info-general" element={<ElBosqueGenInfoPage />} />
+            <Route path="/elbosque-consorcistas" element={<ElBosqueConsorcistasPage />} />
+            <Route path="/elbosque-admin" element={<ElBosqueAdminPage />} />
+          </Route>
+        </Routes>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
+
